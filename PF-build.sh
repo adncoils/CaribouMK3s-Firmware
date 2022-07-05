e080c32690adfce5f270c867030e506cbb26dc24
wschadow <wolfgang.schadow@t-online.de>
2022-07-05 13:31:51 +0200

    adds restoration of BUILD_HASH in Firmware/Configuration.h

--- PF-build.sh
+++ PF-build.sh
@@ -1025,6 +1025,7 @@ prepare_code_for_compiling()
     FW_REVISION=$(grep --max-count=1 "\bFW_REVISION\b" $SCRIPT_PATH/Firmware/Configuration.h| sed -e's/  */ /g'|cut -d ' ' -f3)
     FW="$FW_MAJOR$FW_MINOR$FW_REVISION"
     #FW=$(grep --max-count=1 "\bFW_VERSION\b" $SCRIPT_PATH/Firmware/Configuration.h | sed -e's/  */ /g'|cut -d '"' -f2|sed 's/\.//g')
+    BUILDHASH=$(grep --max-count=1 "\bFW_COMMIT_HASH\b" $SCRIPT_PATH/Firmware/Configuration.h | sed -e's/  */ /g'|cut -d ' ' -f3)
     if [ -z "$BUILD" ] ; then
         # Find build version in Configuration.h file and use it to generate the hex filename
         BUILD=$(grep --max-count=1 "\bFW_COMMIT_NR\b" $SCRIPT_PATH/Firmware/Configuration.h | sed -e's/  */ /g'|cut -d ' ' -f3)
@@ -1444,6 +1445,7 @@ cleanup_firmware()
     # Restore files to previous state
     sed -i -- "s/^#define FW_DEV_VERSION FW_VERSION_$DEV_STATUS/#define FW_DEV_VERSION FW_VERSION_UNKNOWN/g" $SCRIPT_PATH/Firmware/Configuration.h
     sed -i -- 's/^#define FW_REPOSITORY "Prusa3d"/#define FW_REPOSITORY "Unknown"/g' $SCRIPT_PATH/Firmware/Configuration.h
+    sed -i -- "s/^#define FW_COMMIT_HASH.*/#define FW_COMMIT_HASH $BUILDHASH/g" $SCRIPT_PATH/Firmware/Configuration.h
     if [ ! -z "$BUILD_ORG" ] ; then
         sed -i -- "s/^#define FW_COMMIT_NR.*/#define FW_COMMIT_NR $BUILD_ORG/g" $SCRIPT_PATH/Firmware/Configuration.h
 		sed -i -- "s/.*#define FW_COMMIT_HASH.*/#define FW_COMMIT_HASH \"\"/g" $SCRIPT_PATH/Firmware/Configuration.h
